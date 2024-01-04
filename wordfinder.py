4325488fbb19cfde14be0b6c2fe200df8b238998
William <mailliwrd@gmail.com>
2024-01-03 20:52:43 -0700

    commitin 2

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,17 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    def __init__(self, path):
+
+        word_path = open(path)
+
+        self.words = self.parse(word_path)
+
+        print(f"{len(self.words)} words read")
+
+    def parse(self, word_path):
+        return [w.strip() for w in word_path]
+    
+    def random(self):
+        return random.choice(self.words)
